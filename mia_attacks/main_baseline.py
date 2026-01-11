e63a3af7bf213191a2977215ccd20e0dca23bf82
nielstomassen <n.h.c.tomassen@student.tudelft.nl>
2026-01-11 15:11:09 +0100

    Added victim id to cached index files to ensure unique indices per victim.

--- mia_attacks/main_baseline.py
+++ mia_attacks/main_baseline.py
@@ -52,6 +52,7 @@ def run_mia_attack(
     target_model,
     train_loader,
     test_loader,
+    victim_id,
     device: str | torch.device,
     choice: str = "loss",
     measurement_number: int = 10,
@@ -117,7 +118,7 @@ def run_mia_attack(
     num_samples_test = int(0.0 * len(test_images)) # 0
 
     # Training side (save to disk for reproducibility, but regenerate if size changed)
-    idx_path_train = "results_mia/indices/original_indices"
+    idx_path_train = f"results_mia/indices/original_indices_v{victim_id}"
     # Ensure parent directory exists
     os.makedirs(os.path.dirname(idx_path_train), exist_ok=True)
 
@@ -142,7 +143,7 @@ def run_mia_attack(
     measurement_train_images = train_images[anti_indices_train]
     measurement_train_labels = train_labels[anti_indices_train]
 
-    idx_path_test = "results_mia/indices/original_indices_test"
+    idx_path_test  = f"results_mia/indices/original_indices_test_v{victim_id}"
 
     if not os.path.exists(idx_path_test):
         original_indices_test = torch.randperm(len(test_images))
